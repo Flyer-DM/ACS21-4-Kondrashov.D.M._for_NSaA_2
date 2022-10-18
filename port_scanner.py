1467512f8a93580f3af8636d34048b512d521b5e
Dmitry <lexa.gorky@yandex.ru>
2022-10-18 10:37:21 +0300

    port_scanner made

--- port_scanner.py
+++ port_scanner.py
@@ -1,23 +1,32 @@
 import socket
 import threading
+import time
+import re
 
 
 def scanner(hostname: str, port: str) -> None:
-    open_ports = []
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket()
     sock.settimeout(0.5)
     try:
         sock.connect((hostname, port))
         print(f"Порт {port} свободен")
-        open_ports.append(port)
-    except (ConnectionRefusedError, socket.timeout):
+    except (ConnectionRefusedError, socket.timeout, ConnectionError, OSError):
         pass
     finally:
         sock.close()
 
 
 
-hostname = '192.168.0.1'
-for i in range(1, 1000):
-    p = threading.Thread(target=scanner, args=(hostname, i))
-    p.start()
+hostname = input("Введите адрес хоста: ")
+while True:
+    if re.match('^localhost|(\d{3}\.\d{3}\.\d{3}\.\d{3})$', hostname):
+        break
+    hostname = input("Неверный формат адреса, попробуйте ещё раз: ")
+start_time = time.time()
+for i in range(1, 65536):
+    try:
+        p = threading.Thread(target=scanner, args=(hostname, i))
+        p.start()
+    except(RuntimeError):
+        continue
+print("Время работы программы:", round(time.time() - start_time))

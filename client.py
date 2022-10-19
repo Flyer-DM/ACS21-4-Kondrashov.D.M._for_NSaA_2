9e2e56f7869ca8ad328fe41672d8512761b3a792
Dmitry <lexa.gorky@yandex.ru>
2022-10-19 11:08:06 +0300

    authorization on server added with log files, password encryption

--- client.py
+++ client.py
@@ -1,2 +1,25 @@
 import socket
 import threading
+
+
+def listen(s: socket.socket):
+    while True:
+        message = s.recv(1024).decode()
+        if message.startswith('b$'):
+            print('\r\r' + message[2:])
+            break
+        print('\r\r' + message + '\n' + 'you: ', end='')
+
+
+def connect(host: str = '127.0.0.1', port: int = 1025):
+    s = socket.socket()
+
+    s.connect((host, port))
+
+    threading.Thread(target=listen, args=(s,), daemon=True).start()
+
+    while True:
+        message = input('you: ')
+        s.send(message.encode())
+
+connect()
